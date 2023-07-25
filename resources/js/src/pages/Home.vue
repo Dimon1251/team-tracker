e051a77cb73f141cc482c574ff65c3eb801711b7
Ndrmn <andrkult@gmail.com>
2023-07-25 17:52:22 +0200

    base structure

---
+++ resources/js/src/pages/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Главная</h1>
+    <router-link to="/login">Login</router-link>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ resources/js/src/pages/Login.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Логин</h1>
+    <router-link to="/">На главную</router-link>
+</template>
+
+<script>
+export default {
+
+}
+</script>
