59cc79ed750112d90632bb26b2ecbd70f7fe1e72
Dimon1251 <qawe1231231asdcasd@gmail.com>
2023-08-09 22:13:34 +0300

    Crud

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\TicketService;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    //
+    public function __construct(private readonly TicketService $ticketService){}
+
+    public function index(){
+        $tickets = $this->ticketService->all();
+        return response()->json([
+            'Tickets' => $tickets,
+        ]);
+    }
+
+    public function show(Request $request){
+        $ticket = $this->ticketService->find($request->id);
+        return response()->json([
+            'Ticket' => $ticket,
+        ]);
+    }
+
+    public function create(Request $request){
+        $this->ticketService->create($request->toArray());
+    }
+
+    public function update(Request $request){
+        $this->ticketService->update($request->toArray(), $request->id);
+    }
+
+    public function destroy(Request $request){
+        $this->ticketService->delete($request->id);
+    }
+
+/*    public function assignUser(Request $request){
+        $this->ticketService->assignUser($request->ticket_id,$request->user_id_assign, $request->user_id_assigned);
+    }*/
 }
--- app/Repositories/TicketRepository.php
+++ app/Repositories/TicketRepository.php
@@ -11,4 +11,11 @@ class TicketRepository extends Repository
         parent::__construct($model);
     }
 
+ /*   public function assignUser($ticket_id, $assign, $assigned){
+        Ticket::where('id', $ticket_id)->update([
+            'assignee_user_id' => $assign,
+            'assigned_user_id' => $assigned
+        ]);
+    }*/
+
 }
